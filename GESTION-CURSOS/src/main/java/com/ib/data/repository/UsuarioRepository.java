84a37b3905403cb33deac6854e00391e611848c0
Diego Cairone <diegocairone@gmail.com>
2020-08-27 16:41:27 -0300

    Preparacion 4

--- GESTION-CURSOS/src/main/java/com/ib/data/repository/UsuarioRepository.java
+++ GESTION-CURSOS/src/main/java/com/ib/data/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.ib.data.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.ib.data.entity.UsuarioEntity;
@@ -7,9 +9,41 @@ import com.ib.data.entity.UsuarioEntity;
 @Repository
 public class UsuarioRepository implements CrudRepository<UsuarioEntity> {
 
+    private String SQL_SELECT = "SELECT * FROM usuarios";
+    
+    @Autowired
+    private JdbcTemplate template;
+    
     @Override
     public Iterable<UsuarioEntity> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        
+        return template.query(SQL_SELECT, (rs, i) -> {
+            
+            Long id = rs.getLong("usuario_id");
+            String username = rs.getString("username");
+            String password = rs.getString("password");
+            String apellidos = rs.getString("apellidos");
+            String nombres = rs.getString("nombres");
+            String email = rs.getString("email");
+            Boolean accountNonExpired = rs.getBoolean("account_non_expired");
+            Boolean accountNonLocked = rs.getBoolean("is_account_non_locked");
+            Boolean credentialsNonExpired = rs.getBoolean("is_credentials_non_expired");
+            Boolean enabled = rs.getBoolean("is_enabled");
+            
+            UsuarioEntity usuarioEntity = new UsuarioEntity();
+            
+            usuarioEntity.setId(id);
+            usuarioEntity.setUsername(username);
+            usuarioEntity.setPassword(password);
+            usuarioEntity.setNombres(nombres);
+            usuarioEntity.setApellidos(apellidos);
+            usuarioEntity.setEmail(email);
+            usuarioEntity.setAccountNonExpired(accountNonExpired);
+            usuarioEntity.setCredentialsNonExpired(credentialsNonExpired);
+            usuarioEntity.setAccountNonLocked(accountNonLocked);
+            usuarioEntity.setEnabled(enabled);
+            
+            return usuarioEntity;
+        });
     }
 }
--- GESTION-CURSOS/src/main/java/com/ib/data/service/UsuarioService.java
+++ GESTION-CURSOS/src/main/java/com/ib/data/service/UsuarioService.java
@@ -1,18 +1,19 @@
 package com.ib.data.service;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ib.data.entity.UsuarioEntity;
+import com.ib.data.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
 
+    @Autowired private UsuarioRepository usuarioRepository;
+    
     public List<UsuarioEntity> findAll() {
-        return Arrays.asList(
-                new UsuarioEntity(1L, "usuario.prueba", "123", "PEREZ", "JUAN", "juan@perez.com"),
-                new UsuarioEntity(2L, "usuario.dos", "abc", "OTRO", "JUAN", "otro@perez.com"));
+        return (List<UsuarioEntity>) usuarioRepository.findAll();
     }
 }
